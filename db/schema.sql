f88110a83cb22d41ad6437c01622e0b25d3bb1f2
christian6404 <christian6404@gmail.com>
2023-05-31 18:16:27 -0400

    finished database fixed bugs with seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,34 +3,39 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-USE ecommerce_db;
+-- USE ecommerce_db;
+-- DROP TABLE IF EXISTS producttag;
 
-CREATE TABLE category (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    category_name TEXT NOT NULL
-);
+-- DROP TABLE IF EXISTS tag;
 
-CREATE TABLE product (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_name TEXT NOT NULL,
-    price DEC NOT NULL,
-    category_id INT,
-    FOREIGN KEY(category_id)
-    REFERENCES category(id)
-);
+-- CREATE TABLE category (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     category_name TEXT NOT NULL
+-- );
 
-CREATE TABLE tag (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tag_name TEXT
-);
+-- CREATE TABLE product (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     product_name TEXT NOT NULL,
+--     price DEC NOT NULL,
+--     category_id INT,
+--     FOREIGN KEY(category_id)
+--     REFERENCES category(id)
+-- );
 
-CREATE TABLE productTag (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
-    tag_id INT,
-    FOREIGN KEY(product_id)
-    REFERENCES product(id),
+-- CREATE TABLE tag (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     tag_name TEXT
+-- );
 
-    FOREIGN KEY(tag_id)
-    REFERENCES tag(id)
-);
+-- CREATE TABLE productTag (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     product_id INT,
+--     tag_id INT,
+--     FOREIGN KEY(product_id)
+--     REFERENCES product(id)
+--     ON DELETE CASCADE,
+
+--     FOREIGN KEY(tag_id)
+--     REFERENCES tag(id)
+--     ON DELETE CASCADE
+-- );
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('sequelize');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3026;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+// })
